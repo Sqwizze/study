883b548a485867ad2e6fff9e84b4e447c919e054
Stanislav Bondarenko <ssergebondarenko@sberbank.ru>
2022-08-03 12:16:15 +0300

    C0. Initial commit. Main function

---
+++ Math.java
@@ -0,0 +1,8 @@
+public class Math {
+  public static void main (String[] args) {
+    Integer a = Integer.valueOf(args[0]);
+    String actions = args[1];
+    Integer b = Integer.valueOf(args[2]);
+    }
+}
+
