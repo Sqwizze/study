095e1871bfef5a7e2937cea7491e5f9895f7f876
Stanislav Bondarenko <ssergebondarenko@sberbank.ru>
2022-08-03 12:19:56 +0300

    C1. Subtraction function.

--- Math.java
+++ Math.java
@@ -3,6 +3,9 @@ public class Math {
     Integer a = Integer.valueOf(args[0]);
     String actions = args[1];
     Integer b = Integer.valueOf(args[2]);
+  }
+     public static Integer subtraction (Integer a, Integer b){
+    return a*b;
     }
 }
 
