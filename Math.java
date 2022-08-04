9d6fa9ca971a8676be98c4ec13ac8eb9ea465367
Stanislav Bondarenko <ssergebondarenko@sberbank.ru>
2022-08-04 18:19:39 +0300

    C4. Fix Subtraction.

--- Math.java
+++ Math.java
@@ -1,17 +1,18 @@
 public class Math {
-  public static void main (String[] args) {
-    Integer a = Integer.valueOf(args[0]);
-    String actions = args[1];
-    Integer b = Integer.valueOf(args[2]);
-  switch (action){
-      case "-":
-        System.out.printf("Result: %d\n", subtraction(a,b));
-        break;
-      default;
-        System.out.printf("Unknown operator: %s\n, action);
-        break;
-    }}
-     public static Integer subtraction (Integer a, Integer b){
-    return a*b;
+	public static void main (String[] args) {
+		Integer a = Integer.valueOf(args[0]);
+		String action = args[1];
+		Integer b = Integer.valueOf(args[2]);
+	switch (action){
+		case "-":
+			System.out.printf("Result: %d\n", subtraction(a,b));
+			break;
+		default:
+			System.out.printf("Unknown operator: %s\n", action);
+			break;
+    }
+}
+       public static Integer subtraction (Integer a, Integer b){
+	       return a-b;
     }
 }
