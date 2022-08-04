f763f72501de2dc32b232455ee3ab33824474836
Stanislav Bondarenko <ssergebondarenko@sberbank.ru>
2022-08-04 18:38:50 +0300

    C5. Fix merging fucks.

--- Math.java
+++ Math.java
@@ -1,5 +1,4 @@
 public class Math {
-<<<<<<< HEAD
   public static void main (String[] args) {
     Integer a = Integer.valueOf(args[0]);
     String action = args[1];
@@ -17,26 +16,9 @@ public class Math {
   } 
   }
      public static Integer subtraction (Integer a, Integer b){
-    return a*b;
-=======
-	public static void main (String[] args) {
-		Integer a = Integer.valueOf(args[0]);
-		String action = args[1];
-		Integer b = Integer.valueOf(args[2]);
-	switch (action){
-		case "-":
-			System.out.printf("Result: %d\n", subtraction(a,b));
-			break;
-		default:
-			System.out.printf("Unknown operator: %s\n", action);
-			break;
-    }
-}
-       public static Integer subtraction (Integer a, Integer b){
-	       return a-b;
->>>>>>> hotfix
-    }
-    public static Integer addition (Integer a, Integer b){
-      return a+b; 
+    return a-b;
+     }
+     public static Integer addition (Integer a, Integer b){
+      return a+b;
     }
 }
